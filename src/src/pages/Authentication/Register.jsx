516e2b04fe04b16826a1b66d7ff24f7a34e146a7
unknown <edgarrios412@gmail.com>
2023-12-12 16:13:54 -0500

    push

--- src/src/pages/Authentication/Register.jsx
+++ src/src/pages/Authentication/Register.jsx
@@ -107,7 +107,7 @@ export default () => {
                             </button>
                             <input
                                 type={isPasswordHidden ? "password" : "text"}
-                                placeholder="Ingresa tu contraseña"
+                                // placeholder="Ingresa tu contraseña"
                                 className="w-full pr-12 pl-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                             />
                         </div>
@@ -136,7 +136,7 @@ export default () => {
                             </button>
                             <input
                                 type={isPasswordHidden ? "password" : "text"}
-                                placeholder="Ingresa nuevamente tu contraseña"
+                                // placeholder="Ingresa nuevamente tu contraseña"
                                 className="w-full pr-12 pl-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
                             />
                         </div>
